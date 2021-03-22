3eacb7ce8e5b318df5273f908faff860ea8dc17a
zhuzq <zhuzq@92015041000001.ads.36.cn>
2021-03-22 14:01:29 +0800

    update

--- src/com/artcweb/enums/NailTypeEnum.java
+++ src/com/artcweb/enums/NailTypeEnum.java
@@ -1,5 +1,6 @@
 package com.artcweb.enums;
 
+
 public enum NailTypeEnum {
 	SMALL(1,"小钉"),
 	ROSE(2,"玫瑰钉"),
@@ -29,6 +30,59 @@ public enum NailTypeEnum {
 
 	public void setValue(Integer value) {
 		this.value = value;
+	}
+	
+	
+	/**
+	 * 根据value获取到枚举类名称
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public static String getNameByValue(Integer value) {
+		NailTypeEnum type = getByValue(value);
+		return null == type ? "" : type.name();
+	}
+
+	/**
+	 * 根据value获取到枚举类显示名称
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public static String getDisplayNameByValue(Integer value) {
+		NailTypeEnum type = getByValue(value);
+		return null == type ? "" : type.getDisplayName();
+	}
+
+	/**
+	 * 根据value获取枚举类型
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public static NailTypeEnum getByValue(Integer value) {
+		if (null != value) {
+			for (NailTypeEnum type : NailTypeEnum.values()) {
+				if (type.getValue() == value) {
+					return type;
+				}
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 根据value获取枚举name
+	 * @param name
+	 * @return
+	 */
+	public static int getValueByName(String name) {
+		if (null != name && "".equals(name.trim())) {
+			NailTypeEnum type = NailTypeEnum.valueOf(name);
+			return null == type ? 0 : type.value;
+		}
+		return 0;
 	}
 	
 }
